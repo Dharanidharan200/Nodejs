69ad7ee99680f541954bb9414dba45df51ae97d7
Dharanidharan <dharanidharan.h@sandsindia.com>
2023-11-23 09:51:38 +0530

    Complete Tree Strcuture code

--- cygnus_treestructure.js
+++ cygnus_treestructure.js
@@ -1,128 +1,97 @@
-// // const meter_relation = require("./sam.json")
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
 const cors = require('cors')
-const { pool } = require("../config/config")
+const pool = require("../connection/connection")
 router.use(cors())
 router.use(cors());
 router.use(bodyParser.json());
 
+//new tree strcuture
 exports.get_treestructure = async (req, res) => {
     try {
         const company_id = req.body.c_id
         let meter_relation = await pool.query(`SELECT * FROM sch_mstr_data.meter_relationship WHERE c_id='${company_id}'`)
-        meter_relation=meter_relation.rows
-        // console.log(meter_relation);
+        meter_relation = meter_relation.rows
+        let finaldata = [];
         for (let i = 0; i < meter_relation.length; i++) {
-            const item = meter_relation[i];
-          
+            item = meter_relation[i];
+
             const mData = item.slave_id;
-        
-        
+
+
             try {
                 const raw_data = await pool.query(
                     `SELECT * FROM sch_raw_data.data_rt_raw_t1 WHERE slave_id='${mData}' ORDER BY mfm_read_time DESC LIMIT 1`
                 );
-                
+
                 const rawDataItem = raw_data.rows[0];
-        
+
                 if (rawDataItem) {
                     item.last_updated_dttm = rawDataItem.mfm_read_time;
                     item.p10 = rawDataItem.p10;
                     item.p32 = rawDataItem.p32;
-                    item.p33 = rawDataItem.p33;
-        
+                    item.p4 = rawDataItem.p4;
+
                     const current_date = new Date();
                     const timeDifference = current_date - item.last_updated_dttm;
                     const hoursDifference = timeDifference / (1000 * 60 * 60);
-        
+
                     if (hoursDifference < 1) {
                         item.status = "on";
                     } else {
                         item.status = "E1";
                     }
                 }
+                finaldata.push(item)
+
             } catch (error) {
                 console.error(`Error fetching data for slave_id '${mData}': ${error}`);
                 // Handle the error or continue the loop as needed.
             }
         }
-        
-            console.log(meter_relation);
-          
-            const tree = [];
-            const nodeMap = {};
 
-            for (let i = 0; i < meter_relation.length; i++) {
-                const item = meter_relation[i];
-                const newNode = { name: item.slave_name, children: [] };
+        //Tree Strcutre function
+        const buildTreeFlat = (data, parent_id) => {
+            const result = [];
 
-                if (item.parent_id === "root_node") {
-                    tree.push(newNode);
-                } else {
-                    const parentNode = nodeMap[item.parent_id];
-                    if (parentNode) {
-                        parentNode.children.push(newNode);
+            for (const item of data) {
+                if (item.parent_id == parent_id || (item.parent_id == 'root-node' && parent_id == null)) {
+                    const node = {
+                        name: `${item.slave_name}`,
+                        id: item.slave_id
+                    };
+
+                    // Check if the node has already been processed to avoid infinite recursion
+                    const alreadyProcessed = result.some(existingNode => existingNode.id === node.id);
+                    if (!alreadyProcessed) {
+                        const children = buildTreeFlat(data, item.slave_id);
+                        if (children.length > 0) {
+                            node.children = children;
+                        } else {
+                            node.value = [
+                                { KW: item.p32 },
+                                { A: item.p4 },
+                                { PF: item.p10 },
+                                { SS: item.status }
+                            ];
+                        }
+
+                        result.push(node);
                     }
                 }
-
-                nodeMap[item.slave_id] = newNode;
             }
 
+            return result;
+        };
 
+        const resultFlat = buildTreeFlat(finaldata, null);
 
-            const updateJSON = (data) => {
-                data.forEach((node) => {
-
-
-                    if (node.children && node.children.length === 0) {
-                        // node.value = 0;
-                        delete node.children;
-                        if (node.value == 0) {
-                            node.lineStyle = {
-                                color: 'red',
-                            };
-                            node.itemStyle = {
-                                borderColor: 'red'
-                            };
-                        }
-                        else {
-                            node.value = [
-                                { KW: 0 },
-                                { A: 0 },
-                                { PF: 0 }, // Initialize these values with 0 or null
-                                { SS: 0 },
-                            ];
-
-                            // Loop through meter_relation to find the corresponding data
-                            for (let i = 0; i < meter_relation.length; i++) {
-                                if (node.name === meter_relation[i].slave_name) {
-                                    node.value[0].KW = meter_relation[i].p32;
-                                    node.value[1].A = meter_relation[i].p33;
-                                    node.value[2].PF = meter_relation[i].p10;
-                                    node.value[3].SS = meter_relation[i].status;
-                                }
-                            }
-
-                        }
-                    }
-                    if (node.children && node.children.length > 0) {
-                        updateJSON(node.children);
-                    }
-
-                });
-            };
-
-            updateJSON(tree);
-
-            res.send(JSON.stringify(tree, null, 2));
-        } catch (error) {
-            console.log(error);
-            res.status(400).send({ "error": error })
-        }
+        res.send(JSON.stringify(resultFlat, null, 2))
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({ "error": error })
     }
 
-
-
+}
 
